141de2136492f2710aebb8b2c435fad3d352a45b
John C Barstow <jbowtie@amathaine.com>
2015-08-24 21:59:20 +1200

    Add support for moves on second page

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -8,4 +8,10 @@ defmodule Playbook.PageView do
   def starting(moves) do
     Enum.filter(moves, &(&1.level == 1))
   end
+  def advanced(moves) do
+    Enum.filter(moves, &(&1.level == 2))
+  end
+  def master(moves) do
+    Enum.filter(moves, &(&1.level == 6))
+  end
 end
