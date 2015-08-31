6d944c9c501f9dfa7edad2b98baa4c83cd3c6458
John C Barstow <jbowtie@amathaine.com>
2015-08-31 21:09:10 +1200

    Fix presentation of advanced moves

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -6,12 +6,12 @@ defmodule Playbook.PageView do
   end
 
   def starting(moves) do
-    Enum.filter(moves, &(&1.level == 1))
+    Enum.filter(moves, &(&1.level == 1)) |> Enum.sort_by(&(&1.title))
   end
   def advanced(moves) do
-    Enum.filter(moves, &(&1.level == 2))
+    Enum.filter(moves, &(&1.level == 2)) |> Enum.sort_by(&(&1.title))
   end
   def master(moves) do
-    Enum.filter(moves, &(&1.level == 6))
+    Enum.filter(moves, &(&1.level == 6)) |> Enum.sort_by(&(&1.title))
   end
 end
