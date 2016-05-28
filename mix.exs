185a5c645d52c5212e394b5cc710b7d667751278
John C Barstow <jbowtie@amathaine.com>
2016-05-28 19:48:05 +1200

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Playbook.Mixfile do
 
   def project do
     [app: :playbook,
-     version: "0.0.2",
+     version: "0.1.1",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
